3ffc45b2dfb71a80ba92fd83019deff59d67cc4d
JayRodrig <mrjosemlrod@gmail.com>
2019-02-10 22:46:21 -0500

    installed axios and added comments to code all around to make more understandable

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 import Header from './components/header'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pokemon: [],
+    }
+  }
   render() {
     return (
-      <Header />
+      <Header /> // Header component -> ./components/header.js
     );
   }
 }
