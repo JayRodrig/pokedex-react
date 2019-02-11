fd22311345af278a875364d8c9676bc0b152951b
JayRodrig <mrjosemlrod@gmail.com>
2019-02-10 22:06:17 -0500

    completed the header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import './header.css'
 
-const Header = props => {
+const Header = props => {    
     return(
         <>
-            <div className='banner'></div>
-            <div className='container'>
-                <h3 className='title'>Pursuit Pokedex</h3>
-                <input type='text' className='input-field' placeholder='Search...'/>
+            <div className='header'>
+                <div className='banner'></div>
+                <div className='container input-field-container'>
+                        <div className='row'>
+                            <div className='col col-2 img-divs'>
+                                <img src='https://www.freeiconspng.com/uploads/pokeball-icon-20.jpg' alt='pokeball' className='img'/>
+                            </div>
+                            <div className='col col-8'>
+                                <h3 className='title'>Pursuit Pokedex</h3>
+                                <input type='text' placeholder='Search...' className='input-field'/>
+                            </div>
+                            <div className='col col-2 img-divs'>
+                                <img src='https://www.freeiconspng.com/uploads/pokeball-icon-20.jpg' alt='pokeball' className='img'/>
+                            </div>
+                        </div>
+                </div>
             </div>
         </>
     )
